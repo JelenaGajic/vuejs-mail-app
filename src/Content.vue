4bce3d8d4fa79ff20126d11a562891e33bd33a4a
Jelena Gajic <jelenagajic1802@gmail.com>
2018-03-07 14:07:24 +0100

    Implement messages in inbox

--- src/Content.vue
+++ src/Content.vue
@@ -4,7 +4,7 @@
             <h3>{{ currentView.title }}</h3>
         </div>
         <keep-alive>
-            <component :is="currentView.tag"></component>
+            <component :is="currentView.tag" :data="currentView.data"></component>
         </keep-alive>
     </aside>
 </template>
@@ -19,11 +19,18 @@
     import { eventBus } from './main';
 
     export default {
+        props: {
+            messages: {
+                type: Array,
+                required: true
+            }
+        },
         created () {
             eventBus.$on('changeView', (data) => {
                 let temp = [{
                     tag: data.tag,
-                    title: data.title
+                    title: data.title,
+                    data: data.data || {}
                 }];
 
                 this.history = temp.concat(this.history.splice(0));
@@ -35,14 +42,19 @@
                 history: [
                     {
                         tag: 'app-inbox',
-                        title: 'Inbox'
+                        title: 'Inbox',
+                        data: {
+                            messages: null
+                        }
                     }
                 ]
             }
         },
         computed: {
             currentView() {
-                return this.history[0];
+                let current = this.history[0];
+                current.data.messages = this.messages;
+                return current;
             }
         },
         components: {
